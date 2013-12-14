a3ac56f1ec4836e198fc1913a7af708785070ff6
Raymond Lucke <ray@vasaristudio.com>
2013-12-13 16:45:26 -0800

    redis -> resque instance name

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 if ['util'].include?(node[:instance_role])
-  if node[:name] == 'redis'
+  if node[:name] == 'resque'
 
     sysctl "Enable Overcommit Memory" do
       variables 'vm.overcommit_memory' => 1
